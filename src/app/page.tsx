2183667dd19986b5a95cdcd3339903ce4d0a0d31
danrtx <danilo.montezumaib@campusucc.edu.co>
2025-03-14 15:37:05 -0500

    nombre

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-[#c6caff] via-[#a4a8fd] to-[#7061f3]">
       <h1 className="text-7xl font-bold shadow-2xl p-6 rounded-xl border-4 border-[#847ffa] 
       bg-white text-[#7061f3] hover:bg-[#f0f0ff] transition-all duration-500 transform hover:scale-110 font-poppins">
-        DaniloSphere
+        Danilo
       </h1>
     </div>
   );
